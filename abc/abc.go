19bbf7440d37e65e19865a5f26e5da4ed53fa7ab
BorisPlus <boris-plus@main.ru>
2023-05-09 20:49:03 +0300

    * testing standalone

--- abc/abc.go
+++ abc/abc.go
@@ -1,29 +1,5 @@
-package main
+package abc
 
-import "fmt"
-
-func main() {
-	one := 1
-	two := 2
-	three := 3
-	a := []*int{&one, &two, &three}
-	for _, item := range a {
-		fmt.Println("OK", item, *item)
-	}
-
-	fmt.Println()
-	two = *new(int)
-	for _, item := range a {
-		fmt.Println("OK", item, *item)
-	}
-
-	fmt.Println()
-	a[1] = nil
-	for _, item := range a {
-		fmt.Println("OK", item)
-	}
-
-	a = []*int{&one, &two, &three}
-	fmt.Println("A", a[:0])
-	fmt.Println("A", a[:1])
+func Pow2(x int) int {
+	return x * x
 }
